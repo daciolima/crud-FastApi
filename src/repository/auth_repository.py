697d67cf0839ded007c7f46aaea64703c4ccc96c
Dácio Lima <daciolima@MacBook-Pro.local>
2021-09-20 22:33:12 -0300

    feat: refatoração auth_repository

--- src/repository/auth_repository.py
+++ src/repository/auth_repository.py
@@ -1,2 +1,13 @@
+from fastapi import status, HTTPException
 from sqlalchemy.orm import Session
+from src.models import User
+from src.security import Hash
 
+
+def login(auth, db: Session):
+    user = db.query(User).filter(User.username == auth.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Usuário não encontrado.")
+    if not Hash.verify(user.password, auth.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Password não está correto.")
+    return user
--- src/routes/auth_routes.py
+++ src/routes/auth_routes.py
@@ -2,8 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from src.schemas import LoginSchema
 from config.database import session_db
-from src.models import User
-from src.security.hashing import Hash
+from src.repository import auth_repository
 
 routes_auth = APIRouter(
     tags=['Auth']
@@ -12,9 +11,5 @@ routes_auth = APIRouter(
 
 @routes_auth.post('/auth')
 def login(auth: LoginSchema, db: Session = Depends(session_db)):
-    user = db.query(User).filter(User.username == auth.username).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Usuário não encontrado.")
-    if not Hash.verify(user.password, auth.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Password não está correto.")
-    return user
+    return auth_repository.login(auth, db)
+
